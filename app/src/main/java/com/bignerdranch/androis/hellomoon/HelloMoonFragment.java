72f52313828ae4e97ce33b5d0902b88b78dca59e
DSendros <DSendros@gmail.com>
2015-05-25 22:30:38 -0400

    Chapter 13 - Audio Playback Using MediaPlayer

---
+++ app/src/main/java/com/bignerdranch/androis/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,51 @@
+package com.bignerdranch.androis.hellomoon;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+
+/**
+ * Created by dsendros on 5/25/15.
+ */
+public class HelloMoonFragment extends Fragment {
+    private AudioPlayer mPlayer = new AudioPlayer();
+
+    Button mPlayButton;
+    Button mStopButton;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
+
+        mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.play(getActivity());
+            }
+        });
+
+        mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer.stop();
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPlayer.stop();
+    }
+}
